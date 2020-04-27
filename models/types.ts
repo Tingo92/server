9791f229fe2e8f18b92eb04b97a55cc7c7155e12
Trey Stevens <trey.stevens@upchieve.org>
2020-04-27 19:14:10 -0400

    added properties to User type

--- models/types.ts
+++ models/types.ts
@@ -4,4 +4,8 @@ export interface User extends Document {
   calculateElapsedAvailability: (Date) => number;
   availabilityLastModifiedAt: Date;
   elapsedAvailability: number;
+  createdAt: Date;
+  firstname: string;
+  email: string;
+  pastSessions: Array<string>;
 }
