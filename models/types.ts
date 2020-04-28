802c37efd0748581ede0b5e35bde32f088b86e47
Trey Stevens <trey.stevens@upchieve.org>
2020-04-28 12:52:58 -0400

    added createdAt to User model

--- models/types.ts
+++ models/types.ts
@@ -4,4 +4,5 @@ export interface User extends Document {
   calculateElapsedAvailability: (Date) => number;
   availabilityLastModifiedAt: Date;
   elapsedAvailability: number;
+  createdAt: Date;
 }
