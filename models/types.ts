b1e7764bada043de7f1ccd87a2734f5a79b94176
Trey Stevens <trey.stevens@upchieve.org>
2020-04-30 23:15:02 -0400

    updated user interface

--- models/types.ts
+++ models/types.ts
@@ -1,8 +1,18 @@
 import { Document } from 'mongoose';
 
+interface Certification {
+  passed: boolean;
+  tries: number;
+}
+
 export interface User extends Document {
   calculateElapsedAvailability: (Date) => number;
   availabilityLastModifiedAt: Date;
   elapsedAvailability: number;
   createdAt: Date;
+  firstname: string;
+  email: string;
+  certifications: {
+    [key: string]: Certification;
+  };
 }
