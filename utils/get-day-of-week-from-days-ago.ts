1b2e3cd3ac84a39856af7a76f1f8ec36992c3913
Trey Stevens <trey.stevens@upchieve.org>
2020-12-17 00:11:18 -0500

    default daysAgo and export for js files

--- utils/get-day-of-week-from-days-ago.ts
+++ utils/get-day-of-week-from-days-ago.ts
@@ -1,12 +1,15 @@
 import moment from 'moment-timezone';
 import { DAYS } from '../constants';
 
-const getTodaysDay = (): string => {
+const getDayFrom = (daysAgo = 0): string => {
   // @note: a volunteer's availability is currenty being stored in EST timezone
   const day = moment()
     .tz('America/New_York')
+    .subtract(daysAgo, 'days')
     .day();
+
   return DAYS[day];
 };
 
-export default getTodaysDay;
+module.exports = getDayFrom;
+export default getDayFrom;
