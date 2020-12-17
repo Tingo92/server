3b49450df8caa3102eaef3c7c3475681a01d7739
Trey Stevens <trey.stevens@upchieve.org>
2020-12-17 12:51:18 -0500

    fns for latest history, elapsed avail for date range, doc creators

--- services/AvailabilityService.ts
+++ services/AvailabilityService.ts
@@ -1,10 +1,14 @@
 import moment from 'moment-timezone';
+import { Query, Types } from 'mongoose';
 import AvailabilitySnapshotModel, {
-  AvailabilitySnapshot
+  AvailabilitySnapshot,
+  AvailabilitySnapshotDocument
 } from '../models/Availability/Snapshot';
 import AvailabilityHistoryModel, {
-  AvailabilityHistory
+  AvailabilityHistory,
+  AvailabilityHistoryDocument
 } from '../models/Availability/History';
+import { Availability, AvailabilityDay } from '../models/Availability/types';
 import countAvailabilityHours from '../utils/count-availability-hours';
 import removeTimeFromDate from '../utils/remove-time-from-date';
 import getFrequencyOfDays from '../utils/get-frequency-of-days';
@@ -21,6 +25,12 @@ export const getAvailability = (
     .exec();
 };
 
+export const getAvailabilities = (query): Promise<AvailabilitySnapshot[]> => {
+  return AvailabilitySnapshotModel.find(query)
+    .lean()
+    .exec();
+};
+
 export const getAvailabilityHistory = (
   query,
   projection = {}
@@ -31,14 +41,12 @@ export const getAvailabilityHistory = (
     .exec();
 };
 
-// @todo: Create a compound index on createdAt(or date) and volunteerId
+// @todo: Create a compound index on date and volunteerId
 export const getRecentAvailabilityHistory = async (
   volunteerId
 ): Promise<AvailabilityHistory> => {
   const [document] = await AvailabilityHistoryModel.find({ volunteerId })
-    // @todo: decide on createdAt or date
-    // .sort({ date: -1 })
-    .sort({ createdAt: -1 })
+    .sort({ date: -1 })
     .limit(1)
     .lean()
     .exec();
@@ -46,16 +54,82 @@ export const getRecentAvailabilityHistory = async (
   return document;
 };
 
-// Calculates the amount of hours between the last calculation for elapsedAvailability and the current date
+// @todo: Create a compound index on createdAt(or date) and volunteerId
+export const getLatestHistoryFromDaysAgo = async ({
+  volunteerId,
+  daysAgo = 0
+}): Promise<AvailabilityHistory[]> => {
+  const startOfDay = moment
+    .utc()
+    .subtract(daysAgo, 'days')
+    .startOf('day')
+    .format();
+  const endOfDay = moment
+    .utc()
+    .subtract(daysAgo, 'days')
+    .endOf('day')
+    .format();
+
+  return AvailabilityHistoryModel.find({
+    volunteerId,
+    date: { $gte: new Date(startOfDay), $lte: new Date(endOfDay) }
+  })
+    .sort({ date: 1 })
+    .lean()
+    .exec();
+};
+
+export const getElapsedAvailabilityForDateRange = async (
+  volunteerId,
+  fromDate,
+  toDate
+): Promise<number> => {
+  const [result] = await AvailabilityHistoryModel.aggregate([
+    {
+      $match: {
+        volunteerId,
+        date: {
+          $gte: new Date(fromDate),
+          $lte: new Date(toDate)
+        }
+      }
+    },
+    {
+      $project: {
+        elapsedAvailability: 1,
+        volunteerId: 1
+      }
+    },
+    {
+      $group: {
+        _id: '$volunteerId',
+        totalElapsedAvailability: {
+          $sum: '$elapsedAvailability'
+        }
+      }
+    },
+    {
+      $project: { _id: 0, totalElapsedAvailability: 1 }
+    }
+  ]);
+
+  return result.totalElapsedAvailability;
+};
+
+// Calculates the elapsed availability between two dates
 export const calculateElapsedAvailability = ({
   availability,
-  lastCalculatedAt,
-  currentDate
+  fromDate,
+  toDate
+}: {
+  availability: Availability;
+  fromDate: Date;
+  toDate: Date;
 }): number => {
-  const lastCalculatedAtEst = moment(lastCalculatedAt)
+  const fromDateEst = moment(fromDate)
     .tz('America/New_York')
     .format();
-  const currentDateEst = moment(currentDate)
+  const toDateEst = moment(toDate)
     .tz('America/New_York')
     .format();
 
@@ -66,8 +140,8 @@ export const calculateElapsedAvailability = ({
 
   // Count the occurrence of days of the week between a start and end date
   const frequencyOfDaysList = getFrequencyOfDays(
-    removeTimeFromDate(lastCalculatedAtEst),
-    removeTimeFromDate(currentDateEst)
+    removeTimeFromDate(fromDateEst),
+    removeTimeFromDate(toDateEst)
   );
 
   let totalHours = calculateTotalHours(
@@ -77,11 +151,36 @@ export const calculateElapsedAvailability = ({
 
   // Deduct the amount hours that fall outside of the start and end date time
   const outOfRangeHours = countOutOfRangeHours(
-    lastCalculatedAtEst,
-    currentDateEst,
+    fromDateEst,
+    toDateEst,
     availability
   );
   totalHours -= outOfRangeHours;
 
   return totalHours;
 };
+
+export const createAvailabilitySnapshot = (
+  volunteerId
+): Promise<AvailabilitySnapshotDocument> =>
+  AvailabilitySnapshotModel.create({ volunteerId });
+
+export const updateAvailabilitySnapshot = (
+  volunteerId,
+  update
+): Query<AvailabilitySnapshotDocument> =>
+  AvailabilitySnapshotModel.updateOne(
+    {
+      volunteerId
+    },
+    update
+  );
+
+export const createAvailabilityHistory = (data: {
+  availability: AvailabilityDay;
+  volunteerId: Types.ObjectId;
+  timezone: string;
+  date: Date;
+  elapsedAvailability: number;
+}): Promise<AvailabilityHistoryDocument> =>
+  AvailabilityHistoryModel.create(data);
