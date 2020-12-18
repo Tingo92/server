9508b40cbbec7d11a9c8f0412401d4b388d31abe
Trey Stevens <trey.stevens@upchieve.org>
2020-12-17 19:05:13 -0500

    updated CalendarCtrl tests to test for availability snapshot and history

--- tests/controllers/CalendarCtrl.test.ts
+++ tests/controllers/CalendarCtrl.test.ts
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 import CalendarCtrl from '../../controllers/CalendarCtrl';
-import { insertVolunteer, resetDb } from '../db-utils';
+import {
+  insertAvailabilitySnapshot,
+  insertVolunteer,
+  resetDb
+} from '../db-utils';
 import {
   buildAvailability,
   buildVolunteer,
@@ -10,6 +14,8 @@ import VolunteerModel from '../../models/Volunteer';
 import UserActionModel from '../../models/UserAction';
 import { Volunteer } from '../types';
 import { USER_ACTION, SUBJECTS } from '../../constants';
+import * as AvailabilityService from '../../services/AvailabilityService';
+import getDayOfWeekFromDaysAgo from '../../utils/get-day-of-week-from-days-ago';
 
 // db connection
 beforeAll(async () => {
@@ -54,6 +60,7 @@ describe('Save availability and time zone', () => {
 
   test('Should update availability (and user action fires) not onboarded', async () => {
     const volunteer = await insertVolunteer();
+    await insertAvailabilitySnapshot({ volunteerId: volunteer._id });
     const availability = buildAvailability({
       Saturday: { '1p': true, '2p': true }
     });
@@ -73,12 +80,16 @@ describe('Save availability and time zone', () => {
       .lean()
       .select('availability isOnboarded')
       .exec()) as Volunteer;
+    const availabilitySnapshot = await AvailabilityService.getAvailability({
+      volunteerId: volunteer._id
+    });
     const expectedUserAction = await UserActionModel.findOne({
       user: volunteer._id,
       action: USER_ACTION.ACCOUNT.ONBOARDED
     });
 
     expect(updatedAvailability).toMatchObject(availability);
+    expect(availabilitySnapshot.onCallAvailability).toMatchObject(availability);
     expect(isOnboarded).toBeFalsy();
     expect(expectedUserAction).toBeNull();
   });
@@ -97,6 +108,7 @@ describe('Save availability and time zone', () => {
         ]
       })
     );
+    await insertAvailabilitySnapshot({ volunteerId: volunteer._id });
     const availability = buildAvailability({
       Saturday: { '1p': true, '2p': true }
     });
@@ -120,7 +132,9 @@ describe('Save availability and time zone', () => {
       user: volunteer._id,
       action: USER_ACTION.ACCOUNT.ONBOARDED
     });
-
+    const availabilitySnapshot = await AvailabilityService.getAvailability({
+      volunteerId: volunteer._id
+    });
     const expectedUserAction = {
       user: volunteer._id,
       actionType: USER_ACTION.TYPE.ACCOUNT,
@@ -128,8 +142,60 @@ describe('Save availability and time zone', () => {
     };
 
     expect(updatedAvailability).toMatchObject(availability);
+    expect(availabilitySnapshot.onCallAvailability).toMatchObject(availability);
     expect(isOnboarded).toBeTruthy();
     expect(userAction).toMatchObject(expectedUserAction);
+  });
+
+  test('Should create an availability history snapshot of the current day', async () => {
+    // Setting a spy on Date so that elapsedAvailability is calculated
+    // new Date('2020-12-17T16:00:00.000Z').getTime()
+    const dateNowSpy = jest
+      .spyOn(Date, 'now')
+      .mockImplementation(() => 1608220800000);
+
+    const hawking = await insertVolunteer(
+      buildVolunteer({
+        isOnboarded: true,
+        isApproved: true
+      })
+    );
+    const day = getDayOfWeekFromDaysAgo();
+    const availability = buildAvailability({
+      [day]: { '3p': true, '11p': true }
+    });
+    await insertAvailabilitySnapshot({
+      volunteerId: hawking._id,
+      modifiedAt: new Date('12/17/20'),
+      onCallAvailability: buildAvailability({
+        [day]: { '1p': true, '2p': true, '5p': true, '10p': true }
+      })
+    });
+
+    const input = {
+      user: hawking,
+      tz: 'American/New York',
+      availability
+    };
+    await CalendarCtrl.updateSchedule(input);
+
+    const availabilitySnapshot = await AvailabilityService.getAvailability({
+      volunteerId: hawking._id
+    });
+    const availabilityHistory = await AvailabilityService.getRecentAvailabilityHistory(
+      hawking._id
+    );
+    const expectedElapsedAvailability = 3;
+
+    expect(availabilitySnapshot.onCallAvailability).toMatchObject(availability);
+    expect(availabilitySnapshot.onCallAvailability[day]).toMatchObject(
+      availabilityHistory.availability
+    );
+    expect(availabilityHistory.elapsedAvailability).toEqual(
+      expectedElapsedAvailability
+    );
+
+    dateNowSpy.mockRestore();
   });
 });
 
@@ -144,9 +210,9 @@ describe('Clear schedule', () => {
     const volunteer = await insertVolunteer(
       buildVolunteer({ availability, certifications })
     );
-    const timeZone = 'American/New York';
+    const timezone = 'American/New York';
 
-    await CalendarCtrl.clearSchedule(volunteer, timeZone);
+    await CalendarCtrl.clearSchedule(volunteer, timezone);
 
     const emptyAvailability = buildAvailability();
     const { availability: updatedAvailability } = (await VolunteerModel.findOne(
