72c0239cf26fc37ef1a5c1244b23f25242263ae3
Trey Stevens <trey.stevens@upchieve.org>
2020-12-17 20:28:36 -0500

    batch and use setImmediate to populate collection

--- dbutils/populate-availability-snapshots.ts
+++ dbutils/populate-availability-snapshots.ts
@@ -2,38 +2,10 @@ import mongoose from 'mongoose';
 import dbconnect from './dbconnect';
 import AvailabilitySnapshotModel from '../models/Availability/Snapshot';
 import VolunteerModel from '../models/Volunteer';
+import util from 'util';
+const setImmediatePromise = util.promisify(setImmediate);
 
 const upgrade = async (): Promise<void> => {
-  try {
-    await dbconnect();
-
-    const volunteers: any = await VolunteerModel.find({})
-      .lean()
-      .exec();
-
-    const updates = [];
-    for (const volunteer of volunteers) {
-      updates.push(
-        AvailabilitySnapshotModel.create({
-          volunteerId: volunteer._id,
-          onCallAvailability: volunteer.availability,
-          modifiedAt: volunteer.availabilityLastModifiedAt,
-          createdAt: volunteer.createdAt,
-          timezone: volunteer.timezone
-        })
-      );
-    }
-
-    const results = await Promise.all(updates);
-    console.log(results);
-  } catch (error) {
-    console.error(error);
-  }
-
-  mongoose.disconnect();
-};
-
-const upgradeTwo = async (): Promise<void> => {
   try {
     await dbconnect();
 
@@ -59,14 +31,26 @@ const upgradeTwo = async (): Promise<void> => {
       });
     }
 
-    const results = await AvailabilitySnapshotModel.insertMany(docs);
-    console.log(results);
+    const chunks = [];
+    const chunkSizeLimit = 50;
+    for (let i = 0; i <= Math.floor(docs.length / chunkSizeLimit); i++) {
+      const chunk = docs.slice(i * chunkSizeLimit, chunkSizeLimit * (i + 1));
+      chunks.push(chunk);
+    }
+
+    for (const chunk of chunks) {
+      await setImmediatePromise();
+      console.log('\n\n After IO ~~~');
+      await AvailabilitySnapshotModel.insertMany(chunk);
+      console.log('Inserted!');
+    }
   } catch (error) {
     console.error(error);
   }
-
+  // Only disconnect after uploading has finished
+  await setImmediatePromise();
+  console.log('Disconnecting')
   mongoose.disconnect();
 };
 
-// upgrade();
-upgradeTwo();
+upgrade();
