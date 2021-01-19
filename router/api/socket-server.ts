7ba1103760e2d8cb9b9c03afadc891a5d484c8b2
Dave Sudia <dave.sudia@upchieve.org>
2021-01-19 16:32:17 -0700

    just logs socket.request

--- router/api/socket-server.ts
+++ router/api/socket-server.ts
@@ -1,12 +1,11 @@
 /**
  * Creates the socket server and returns the Server instance
  */
-const http = require('http')
-const socket = require('socket.io')
-const redisAdapter = require('socket.io-redis')
-const config = require('../../config')
-const logger = require('../../logger')
-const expressPino = require('express-pino-logger')
+import http from 'http'
+import socketio from 'socket.io'
+import redisAdapter from 'socket.io-redis'
+import config from '../../config'
+import logger from '../../logger'
 
 // Create an HTTPS server if in production, otherwise use HTTP.
 const createServer = app => {
@@ -25,16 +24,17 @@ module.exports = function(app) {
 
   console.log('Sockets.io listening on port ' + port)
 
-  const io = socket(server)
+  const io = socketio(server)
   if (process.env.NODE_ENV === 'test') return io
 
   const redisUrl = new URL(config.redisConnectionString)
   io.adapter(redisAdapter({ host: redisUrl.hostname, port: redisUrl.port }))
 
   // initiate logging on the socket connections following https://socket.io/docs/v2/namespaces/#Namespace-middleware
-  const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
-  const expressLogger = expressPino({ logger })
-  io.use(wrap(expressLogger))
+  io.use((socket, next) => {
+    logger.info(socket.request)
+    next()
+  })
 
   return io
 }
